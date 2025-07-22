376a4ac7263488b1000bea4192a31ee7df1db76b
garv-cpu <balwanigarv2006@gmail.com>
2025-07-22 20:47:52 +0530

    new updates

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const NavBar = () => {
+  return (
+    <nav className='navbar'>
+        <Link to='/'>
+        <p className='text-2xl font-bold text-gradient'>REZUME</p>
+        </Link>
+        <Link to='/upload' className='primary-button w-fit'>
+            Upload Rezume
+        </Link>
+    </nav>
+  )
+}
+
+export default NavBar
